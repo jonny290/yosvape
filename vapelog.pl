e4635857db0d348381c699f85edecf48f8f6630b
jonny 290 <jonny 290@jonny290-PC.290.ath.cx>
2013-02-28 20:21:16 -0600

    tightened up the graphics on level 3
    oh also cleaned whitespace and added comments for the weird crap

--- vapelog.pl
+++ vapelog.pl
@@ -1,4 +1,9 @@
 #!/usr/bin/perl
+#YOSVape Logger
+#JONNY 290 2013
+#
+# Designed to be kept alive by yosvape.sh.
+#
 
 use Device::SerialPort;
 
@@ -6,11 +11,7 @@ $LOGDIR  = "/vape";           # path to data file
 $LOGFILE = "yosvape.log";     # file name to output to
 $PORT    = "/dev/ttyACM0";    # port to watch
 
-#
-#
 # Serial Settings
-#
-#
 
 $ob = Device::SerialPort->new($PORT) || die "Can't Open $PORT: $!";
 $ob->baudrate(9600)    || die "failed setting baudrate";
@@ -20,24 +21,15 @@ $ob->handshake("none") || die "failed setting handshake";
 $ob->dtr_active(0)     || die "failed setting dtr_active";
 $ob->write_settings    || die "no settings";
 
-#
-# open the logfile, and Port
-#
-
-open( LOG, ">>${LOGDIR}/${LOGFILE}" )
-  || die "can't open smdr file $LOGDIR/$LOGFILE for append: $SUB $!\n";
-
-#select(LOG),
+open( LOG, ">>${LOGDIR}/${LOGFILE}" )  || die "can't open smdr file $LOGDIR/$LOGFILE for append: $SUB $!\n";
 $| = 1;    # set nonbufferd mode
 
-#
 # Loop forver, logging data to the log file
-#
 
-$ob->are_match("\n");
+$ob->are_match("\n"); #This sets the characters we consider a record separator, basically
 my $line;
-while (1) {    # print input device to file
-    $line = $ob->lookfor;
+while (1) {    # dont stop till u get enough
+    $line = $ob->lookfor;   #Returns undef if we havent gotten an are_match - terminated string, or the string if we have
     if ($line) {
         print LOG "$line\n";
         my ( $uptime, $mode, $pwm, $temp, $setpoint ) = split( /,/, $line );
@@ -50,8 +42,6 @@ while (1) {    # print input device to file
             unlink('/vape/futuremode');
         }
     }
-    select( undef, undef, undef, 0.5 );
-
+    select( undef, undef, undef, 0.5 ); #ancient chinese secret for sub-1s naps without time::hires or shelling out
 }
-
 undef $ob;
