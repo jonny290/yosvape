5c4a368c815541ac5c5f4ec2230e8f51c2507c5c
ugexe <nlogan@gmail.com>
2013-03-01 18:08:30 -0800

    More useless configuration cleanup/ordering

--- vapelog.pl
+++ vapelog.pl
@@ -13,32 +13,31 @@ use Getopt::Long::Descriptive;
  
 our $VERSION = '0.000_420';
 
-my $conf_obj = Config::General->new( $opt->config );
-my %conf     = $conf_obj->getall;
-
 # Use Getopt::Long::Descriptive to override default config
 my ($opt, $usage) = describe_options(
     "$0 %o <some-arg>",
-    [ 'config|c:s',  "load config file",                
-        { default => $conf{vapelog}{logdir  } || 'yosvape.conf' }
-    ],
-    [ 'logdir|d:i',  "the port to connect to",          
-        { default => $conf{vapelog}{logdir  } || '/vape'        }
-    ],
-    [ 'logfile|f:s', "the nick for yosvape to use",     
-        { default => $conf{vapelog}{logfile } || 'yosvape.log'  }
-    ],
-    [ 'port|p:s',    "the port for some shit",
-        { default => $conf{vapelog}{port    } || '/dev/ttyACM0' }],
+    [   'config|c:s',  "load config file",            { default => 'yosvape.conf' }],
+    [   'logdir|d:i',  "the port to connect to",      { default => '/vape'        }],
+    [   'logfile|f:s', "the nick for yosvape to use", { default => 'yosvape.log'  }],
+    [   'port|p:s',    "the port for some shit",      { default => '/dev/ttyACM0' }],
     [],
-    [ 'help',        "print usage message and exit" ],
+    [   'help',        "print usage message and exit" ],
 );
+
+my $conf_obj   = Config::General->new( $opt->config );
+my %conf_all   = $conf_obj->getall;     # Access all configuration
+my %conf       = %{$conf_all{vapelog}}; # Access vapelog specific configuration
+$conf{logdir}  = $opt->logdir  || $conf{logdir };
+$conf{logfile} = $opt->logfile || $conf{logfile};
+$conf{port}    = $opt->port    || $conf{port   };
+
+
 print($usage->text), exit if $opt->help;
 
-my $LOGFILE = $opt->logdir . '/' . $opt->logfile;
+my $LOGFILE = $conf{logdir} . '/' . $conf{logfile};
 
 # Serial Settings
-my $ob = Device::SerialPort->new($opt->port) || die "Can't Open " . $opt->port . ": $!";
+my $ob = Device::SerialPort->new($conf{port}) || die "Can't Open " . $conf{port} . ": $!";
 $ob->baudrate(9600)    || die "failed setting baudrate";
 $ob->parity("none")    || die "failed setting parity";
 $ob->databits(8)       || die "failed setting databits";
@@ -46,22 +45,21 @@ $ob->handshake("none") || die "failed setting handshake";
 $ob->dtr_active(0)     || die "failed setting dtr_active";
 $ob->write_settings    || die "no settings";
 
+$ob->are_match("\n"); #This sets the characters we consider a record separator, basically
 
 open( my $logfile, '>>', $LOGFILE )  || die "can't open smdr file $LOGFILE for append: $!\n";
 $| = 1;    # set nonbufferd mode
 
 # Loop forver, logging data to the log file
-
-$ob->are_match("\n"); #This sets the characters we consider a record separator, basically
-my $line;
 while (1) {    # dont stop till u get enough
-    if ($line = $ob->lookfor) {   #Returns undef if we havent gotten an are_match - terminated string, or the string if we have
+    if (my $line = $ob->lookfor) {   #Returns undef if we havent gotten an are_match - terminated string, or the string if we have
         print {$logfile} $line;
         my ( $uptime, $mode, $pwm, $temp, $setpoint ) = split( /,/, $line );
-        my $currentmode = $mode;
+
         my $futuremode  = `cat /vape/futuremode`;
         chomp($futuremode);
-        if ( $futuremode && $futuremode ne $currentmode ) {
+
+        if ( $futuremode && $futuremode ne $mode ) {
             $ob->write($futuremode) if ( $futuremode =~ /\w/ );
             print "MODE CHANGE TO $futuremode\n";
             unlink('/vape/futuremode');
@@ -71,4 +69,4 @@ while (1) {    # dont stop till u get enough
 }
 undef $ob;
 
-"420";
+"420" || 1;
